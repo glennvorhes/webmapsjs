f6d1ba3726c89edaf97c06d6245dc63019f823bb
Glenn Vorhes <gavorhes@wisc.edu>
2016-10-03 13:44:17 -0500

    add common projections

---
+++ dist/olHelpers/projections.js
@@ -0,0 +1,9 @@
+"use strict";
+/**
+ * Created by gavorhes on 10/3/2016.
+ */
+var custom_ol_1 = require('custom-ol');
+exports.proj4326 = new custom_ol_1.ol.proj.Projection({ code: 'EPSG:4326' });
+exports.proj3857 = new custom_ol_1.ol.proj.Projection({ code: 'EPSG:3857' });
+exports.proj3070 = new custom_ol_1.ol.proj.Projection({ code: 'EPSG:3070' });
+//# sourceMappingURL=projections.js.map
---
+++ src-ts/olHelpers/projections.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by gavorhes on 10/3/2016.
+ */
+import {ol} from 'custom-ol'
+
+
+export const proj4326 = new ol.proj.Projection({code: 'EPSG:4326'});
+export const proj3857 = new ol.proj.Projection({code: 'EPSG:3857'});
+export const proj3070 = new ol.proj.Projection({code: 'EPSG:3070'});
+
