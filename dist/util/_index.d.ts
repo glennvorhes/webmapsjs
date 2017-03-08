b29d4d637b7477d62489362b1c7420c8115a414e
Glenn Vorhes <gavorhes@wisc.edu>
2017-03-08 11:00:36 -0600

    remove declarations in dist

--- dist/util/_index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by gavorhes on 9/22/2016.
- */
-import * as checkDefinedf from './checkDefined';
-export declare namespace checkDefined {
-    const undefinedOrNull: typeof checkDefinedf.undefinedOrNull;
-    const definedAndNotNull: typeof checkDefinedf.definedAndNotNull;
-}
