809deeeea7428c7e33ecf0c4efafae12dbac8108
Glenn Vorhes <gavorhes@wisc.edu>
2017-03-14 00:17:31 -0500

    tsx to js

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 "use strict";
-let config = require('./webpack.base');
 
 const path = require('path');
 const fs = require('fs');
+let config = require('./webpack.base');
+
 const demosDirectory = path.join(__dirname, 'test/demo');
 const files = fs.readdirSync(demosDirectory);
 
 let entries = {};
 for (let f of files){
-    let key = f.replace(/.ts$/, '.js').replace('.tsx$', '.jsx');
+    let key = f.replace(/.tsx?$/, '.js');
     entries[key] = path.join(demosDirectory, f);
 }
 
