d0e0ea9d7b249c04097f95f24fcb4f2fab6374bd
Glenn Vorhes <gavorhes@wisc.edu>
2017-03-15 02:45:46 -0500

    simplify karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -1,86 +1,21 @@
 // Karma configuration
 // Generated on Wed Mar 08 2017 14:44:59 GMT-0600 (Central Standard Time)
 
-module.exports = function (config) {
+
+module.exports = function(config) {
     config.set({
-
-        // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: '',
-
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine'],
-
-        // list of files / patterns to load in the browser
+        frameworks: ["jasmine", "karma-typescript"],
         files: [
             {pattern: 'node_modules/jquery/dist/jquery.min.js', watched: false},
             {pattern: 'node_modules/custom-ol/lib/index.js', watched: false},
-            'test/specs/spec.ts',
+            {pattern: "test/specs/**/*.ts" }, // *.tsx for React Jsx
+            {pattern: "src/**/*.ts" }, // *.tsx for React Jsx
         ],
-
-
-        // list of files to exclude
-        exclude: [],
-
-
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            'test/specs/spec.ts': ['webpack', 'sourcemap'],
+            "test/specs/**/*.ts": ["karma-typescript"], // *.tsx for React Jsx
+            "src/**/*.ts": ["karma-typescript", 'coverage'], // *.tsx for React Jsx
         },
-
-        webpack: require('./webpack.base.js'),
-
-        webpackMiddleware: {
-            // webpack-dev-middleware configuration
-            // i. e.
-            stats: 'errors-only'
-        },
-
-
-        // test results reporter to use
-        // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
-
-
-        // web server port
-        port: 9876,
-
-
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
-
-
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
-
-
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
-
-        // autoWatchBatchDelay: 100,
-
-
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        // browsers: ['Chrome', 'PhantomJS'],
-        browsers: ['Chrome'],
-        // browsers: ['PhantomJS'],
-
-
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: true,
-
-        // Concurrency level
-        // how many browser should be started simultaneous
-        concurrency: Infinity,
-
-        mime: {
-            'text/x-typescript': ['ts', 'tsx']
-        }
-
-    })
+        reporters: ["progress", "karma-typescript"],
+        browsers: ["Chrome"]
+    });
 };
