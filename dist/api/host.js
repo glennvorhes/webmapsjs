1d0413ad302725a695ddd6a9c7821a278d127cec
Glenn Vorhes <gavorhes@wisc.edu>
2018-06-21 13:53:52 -0500

    only local api for port 8081

--- dist/api/host.js
+++ dist/api/host.js
@@ -1,6 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var isLocal = window.location.href.indexOf('localhost') > -1;
+if (parseInt(window.location.port) !== 8081) {
+    isLocal = false;
+}
 var stage_prod = window.location.pathname.indexOf('webmapsstage') > -1 ? 'webmapsstage' : 'webmaps';
 exports.hostRoot = isLocal ? 'http://localhost:8081' : "https://transportal.cee.wisc.edu/gis/" + stage_prod;
 exports.apiRoot = exports.hostRoot + "/api";
--- src/api/host.ts
+++ src/api/host.ts
@@ -1,5 +1,10 @@
 let isLocal = window.location.href.indexOf('localhost') > -1;
 
+if (parseInt(window.location.port) !== 8081){
+    isLocal = false;
+}
+
+
 let stage_prod = window.location.pathname.indexOf('webmapsstage') > -1 ? 'webmapsstage' : 'webmaps';
 
 export const hostRoot = isLocal ? 'http://localhost:8081' : `https://transportal.cee.wisc.edu/gis/${stage_prod}`;
